c8315a07b22407748cecb44ef6245dab5ef98c30
José de Senna <josespnt1990@gmail.com>
2026-02-11 13:32:13 -0300

    feat: cria solucao com membros estaticos na classe Program

--- CircunferenciaVolumeEsfera/Program.cs
+++ CircunferenciaVolumeEsfera/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace CircunferenciaVolumeEsfera;
+
+class Program
+{
+    static double Pi = 3.14;
+
+    static void Main(string[] args)
+    {
+        Console.Write("Entre com o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double circ = Circunferencia(raio);
+        double volume = Volume(raio);
+
+        Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"Valor de PI: {Pi. ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+
+    static double Circunferencia(double raio)
+    {
+        return 2 * Pi * raio;
+    }
+
+    static double Volume(double raio)
+    {
+        return 4.0 / 3.0 * Pi * raio * raio * raio;
+    }
+}
